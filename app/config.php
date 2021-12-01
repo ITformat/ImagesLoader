107845f0764f76fb4b51171940775b487813a045
DJDAI <y.wongchuen@gmail.com>
2021-12-01 13:32:38 +0700

    เพิ่มการเชื่อมต่อฐานข้อมูล

---
+++ app/config.php
@@ -0,0 +1,6 @@
+<?php
+// Config a database connection
+define('DB_HOST',       'localhost');
+define('DB_USERNAME',   'thaisuper_thaiga');
+define('DB_PASSWORD',   '0957wuML');
+define('DB_NAME',       'thaisuper_thaiga');
