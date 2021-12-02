403cb57bd8277661da779f0f9dc77e34cc9c660d
Yodsapon W <30995391+DJdai@users.noreply.github.com>
2021-12-02 20:08:42 +0700

    Delete config.php

--- app/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Config a database connection
-define('DB_HOST',       'localhost');
-define('DB_USERNAME',   'thaisuper_thaiga');
-define('DB_PASSWORD',   '0957wuML');
-define('DB_NAME',       'thaisuper_thaiga');
